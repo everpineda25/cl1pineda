277579c9c83a7865d1d7e795eaf0f543846d59c9
everpineda25 <pinedaever25@gmail.com>
2023-04-26 23:38:16 -0500

    (final) examencl1

--- src/main/java/cl1pinedaever/Main.java
+++ src/main/java/cl1pinedaever/Main.java
@@ -1,47 +1,69 @@
-package cl1pinedaever;
 
-import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sn = new Scanner(System.in);
-        boolean salir = false;
-        int opcion; //
-        while (!salir) {
- 
-            System.out.println("Ingresar Citas");
-            System.out.println("Mostrar Citas del Doctor");
-            System.out.println("Salir");
- 
-            try {
- 
-                System.out.println("Escribe una de las opciones");
-                opcion = sn.nextInt();
- 
-                switch (opcion) {
-                    case 1:
-                        System.out.println("Ingresar Citas");
-                        break;
-                    case 2:
-                        System.out.println("Mostrar Citas del  Doctor");
-                        break;
-                    case 3:
-                        salir = true;
-                        break;
-                    default:
-                        System.out.println("Opciones entre 1 y 3");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Elija una Opcion");
-                sn.next();
+public class Main {
+    private static EntityManagerFactory emf;
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        emf = Persistence.createEntityManagerFactory("CL1PINEDA");
+        scanner = new Scanner(System.in);
+
+        int opcion;
+        do {
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Ingresar Cita");
+            System.out.println("2. Mostrar Citas del Doctor");
+            System.out.println("3. Salir");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    ingresarCita();
+                    break;
+                case 2:
+                    mostrarCitasDoctor();
+                    break;
+                case 3:
+                    System.out.println("Salir");
+                    break;
+                
             }
-        }
- 
+        } while (opcion != 3);
+
+        emf.close();
+        scanner.close();
     }
 
-	}
+    private static void mostrarCitasDoctor() {
+        EntityManager em = emf.createEntityManager();
+
+        System.out.print("Ingrese el nombre del doctor: ");
+        String nombreDoctor = scanner.nextLine();
+    }
 
 
+	private static void ingresarCita() {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction et = em.getTransaction();
+        et.begin();
+
+        System.out.print("Numero de Cita: ");
+        int nroCita = scanner.nextInt();
+        scanner.nextLine(); 
+        System.out.print("Fecha de la cita: ");
+        String fecha = scanner.nextLine();
+        System.out.print("Nombre del paciente: ");
+        String paciente = scanner.nextLine();
+        System.out.print("Nombre del Doctor: ");
+        String nombreDoctor = scanner.nextLine();
+        
+        
+    }
+}
